38ba62dc52bdbfe176d5d4ece557cefca70330be
Your Name <you@example.com>
2024-08-06 22:56:18 +0330

    customizing the template in nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page-wrapper">
+    <Head> </Head>
+    <the-main-header />
+    <the-mobile-header />
+    <!-- start of page-content -->
+    <main class="page-content">
+      <div class="container">
+        <slot />
+      </div>
+    </main>
+    <!-- end of page-content -->
+
+    <the-footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  link: [{ rel: "stylesheet", href: "/css/dependencies.css" }],
+  script: [
+    { src: "/js/dependencies/bootstrap-slider.min.js" },
+    { src: "/js/dependencies/bootstrap.bundle.min.js" },
+    { src: "/js/dependencies/fancybox.umd.js" },
+    { src: "/js/dependencies/iziToast.min.js" },
+    { src: "/js/dependencies/jquery-3.6.0.min.js" },
+    { src: "/js/dependencies/jquery.countdown.min.js" },
+    { src: "/js/dependencies/jquery.simple.timer.min.js" },
+    { src: "/js/dependencies/nouislider.min.js" },
+    { src: "/js/dependencies/remodal.min.js" },
+    { src: "/js/dependencies/select2.min.js" },
+    { src: "/js/dependencies/simplebar.min.js" },
+    { src: "/js/dependencies/swiper-bundle.min.js" },
+    { src: "/js/dependencies/wNumb.js" },
+    { src: "/js/dependencies/zoomsl.min.js" },
+    { src: "/js/custom.js" },
+    { src: "/js/theme.js" },
+  ],
+});
+</script>
+
+<style scoped>
+</style>
